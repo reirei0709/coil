c9320b946b20586ab63955bf09cb28ead7987099
mirei <usrmii301@gmail.com>
2020-09-16 20:28:31 +0900

    スキルリスト完成

--- app/src/main/java/app/itakura/reirei/imageview/MainActivity.kt
+++ app/src/main/java/app/itakura/reirei/imageview/MainActivity.kt
@@ -3,13 +3,14 @@ package app.itakura.reirei.imageview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import coil.api.load
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val url = "https::/lifeistech.com/materials/images/logo-square.png"
+        val url = "https://life-is-tech.com/materials/images/logo-square.png"
 
         val imageView = findViewById<ImageView>(R.id.imageView)
         imageView.load(url)
